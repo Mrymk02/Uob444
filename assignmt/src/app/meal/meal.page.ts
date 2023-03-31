a87d4f86f4e0bd865f21aa7ac38daee2214c725b
bahaa <blainefire00@gmail.com>
2023-04-01 00:58:34 +0300

    hewoo

---
+++ assignmt/src/app/meal/meal.page.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { Meal } from '../data.service';
+
+@Component({
+  selector: 'app-meal',
+  templateUrl: './meal.page.html',
+  styleUrls: ['./meal.page.scss'],
+})
+export class MealPage implements OnInit {
+
+  public title = '';
+  public imageUrl = '';
+  public ingredients = '';
+  public dietType = '';
+  public calories = '';
+
+  constructor(
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) {}
+
+  ngOnInit() {
+      
+  }
+
+  addMeal() {
+    const meal: Meal = {
+      id: Math.floor(Math.random() * 1000) + 1,
+      title: this.title,
+      imageUrl: this.imageUrl,
+      ingredients: this.ingredients.split(','),
+      dietType: this.dietType,
+      calories: Number(this.calories)
+    };
+
+
+    this.alertCtrl.create({
+      header: 'Meal Added',
+      message: `The meal ${meal.title} has been added to your system.`,
+      buttons: ['OK']
+    }).then(alert => alert.present());
+
+    this.navCtrl.pop();
+  }
+
+}
