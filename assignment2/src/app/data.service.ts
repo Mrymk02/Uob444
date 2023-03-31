75ad6101fce24598b5bab9bcbf685ed1b272cc95
bahaa <blainefire00@gmail.com>
2023-03-31 16:18:54 +0300

    datsrcv

--- assignment2/src/app/data.service.ts
+++ assignment2/src/app/data.service.ts
@@ -13,6 +13,13 @@ export interface ListOfMembers
   subPlanVal: number;
   TotalFees: number;
 }
+export class AddMealPage {
+  title: string = '';
+  imageUrl: string = '';
+  ingredients: string = '';
+  dietType: string = '';
+  calories: number = 0;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -28,6 +35,14 @@ export class DataService
     { name: 'Fahad', age: 21, gender: 'Male', phone: '+966 349837758', diet: 'Normal Diet', dietVal:0, subPlan: '1 month', subPlanVal:100, TotalFees: 100 },
     { name: 'Salem', age: 17, gender: 'Male', phone: '+971 555031121', diet: 'Low Carbs', dietVal:50, subPlan: '3 months', subPlanVal:280, TotalFees: 330 }
   ];
+
+  public meal=
+  [
+    { title: 'Chicken', image:'', ingredients: 'Chicken, Salt, Pepper', dietType: 'Low Fat', calories: 300 },
+    { title: 'Salad', image:'', ingredients: 'Lettuce, Tomato, Cucumber', dietType: 'Low Carbs', calories: 100 },
+    { title: 'Pizza', image:'', ingredients: 'Flour, Tomato Sauce, Cheese', dietType: 'Normal Diet', calories: 500 }
+  ];  
+  
 
   constructor( ) 
   { }
