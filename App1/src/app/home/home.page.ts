944bdfe7104ce14d04cd631acb0dab34aeb8376b
Maryam Kameshki <maryamkameshki02@gmail.com>
2023-12-08 17:11:08 +0300

    Tutorial 1

--- App1/src/app/home/home.page.ts
+++ App1/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,90 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertCtrl: AlertController) {}
 
+  //q3
+  counter=0;
+
+  increment()
+  {
+    this.counter++;
+  }
+
+  //q6
+  List=
+  [
+    {Name:"Maryam" ,Contact:"202002021"}
+  ];
+
+  name="";
+  contact="";
+
+  save()
+  {
+    this.List.push({Name:this.name ,Contact:this.contact});
+
+    //alert("hello "+this.List[1].Name+" your contact "+this.List[1].Contact+" is saved successfully");
+  }
+  
+  alertButton=["OK"];
+
+  //q11
+  remove(index=0)
+  {
+    this.List.splice(index,1);
+  }
+
+  //q12
+  async saveIndex(ei=0)
+  {
+    let alert = await this.alertCtrl.create
+    ({
+      header: 'Alert',
+      message: 'Enter New Name:',
+      inputs:[{ name: 'Name',  placeholder: 'New Name',  type: 'text'}],
+      buttons: 
+      [{
+        text: 'Cancel',
+        handler: () => 
+        {
+          //nothing
+        },
+      },
+      {
+        text: 'OK',
+        handler: (data) => 
+        {
+          //changes name
+          this.List[ei].Name=data.Name;
+        },
+      }]
+    });
+alert.present();
+
+  }
+
+  // alertInputs=[""];
+  // alertButtons= 
+  // [
+  //   {
+  //     text: 'Cancel',
+  //     role: 'cancel',
+  //       handler: () => 
+  //       {
+  //         //nothing
+  //       },
+  //   },
+  //   {
+  //     text: 'OK',
+  //     role: 'confirm',
+  //       handler: () => 
+  //       {
+  //         //changes name
+  //         this.List[this.i].Name=this.alertInputs[0];
+  //       },
+  //   },
+  //  ];
+
+  
 }
