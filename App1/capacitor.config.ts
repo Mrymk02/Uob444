4725c7806f2b18a1a6ed3018354679afc04bab44
Maryam Kameshki <maryamkameshki02@gmail.com>
2023-12-14 10:39:49 +0300

    changes reverted

--- App1/capacitor.config.ts
+++ App1/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'App1',
+  appName: 'app1',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- App1/src/app/app.module.ts
+++ App1/src/app/app.module.ts
@@ -7,21 +7,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: 
-  [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule,
-    // initialize angularfire with credentials from the dashboard
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    // Import the AngularFireDatabaseModule to use database
-    provideFirestore(() => getFirestore()),
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
