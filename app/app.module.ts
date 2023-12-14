318e340e708f69e1363b6b89337f60175c3ca069
Maryam Kameshki <maryamkameshki02@gmail.com>
2023-12-14 10:29:22 +0300

    will revert soon

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,6 +10,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAHaBSXAp0JQlkIyiFuLcsZTTJtQvVMqVY",
+  authDomain: "coursesexample-4525f.firebaseapp.com",
+  projectId: "coursesexample-4525f",
+  storageBucket: "coursesexample-4525f.appspot.com",
+  messagingSenderId: "1084371588244",
+  appId: "1:1084371588244:web:f6b6cd3cb8c3f82d3fd081",
+  measurementId: "G-8ZRJ9RLSGK"
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: 
