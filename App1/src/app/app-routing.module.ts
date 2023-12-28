087f10446f31e9c20b63887ff194c901c2ca154d
Maryam Kameshki <maryamkameshki02@gmail.com>
2023-12-28 20:52:04 +0300

    New Empty Project

--- App1/src/app/app-routing.module.ts
+++ App1/src/app/app-routing.module.ts
@@ -11,30 +11,6 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'members',
-    loadChildren: () => import('./members/members.module').then( m => m.MembersPageModule)
-  },
-  {
-    path: 'activities',
-    loadChildren: () => import('./activities/activities.module').then( m => m.ActivitiesPageModule)
-  },
-  {
-    path: 'sign',
-    loadChildren: () => import('./sign/sign.module').then( m => m.SignPageModule)
-  },
-  {
-    path: 'registered/:id',
-    loadChildren: () => import('./registered/registered.module').then( m => m.RegisteredPageModule)
-  },
-  {
-    path: 'view-activity',
-    loadChildren: () => import('./view-activity/view-activity.module').then( m => m.ViewActivityPageModule)
-  },
-  {
-    path: 'view-members',
-    loadChildren: () => import('./view-members/view-members.module').then( m => m.ViewMembersPageModule)
-  },
 ];
 
 @NgModule({
--- App1/src/app/app.module.ts
+++ App1/src/app/app.module.ts
@@ -6,32 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD3OA5JjOt4onPeE0ijSnKMkT_oxCEg5CE",
-  authDomain: "itcs444-6ddaa.firebaseapp.com",
-  projectId: "itcs444-6ddaa",
-  storageBucket: "itcs444-6ddaa.appspot.com",
-  messagingSenderId: "1067801077494",
-  appId: "1:1067801077494:web:47affa1085a39a581daed7",
-  measurementId: "G-42SM8B12PP"
-};
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-  // initialize angularfire with credentials from the dashboard
-  provideFirebaseApp(() => initializeApp(firebaseConfig)),
-  // Import the AngularFireDatabaseModule to use database
-  provideFirestore(() => getFirestore()),
-  provideAuth(() => getAuth())
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
-  
 })
 export class AppModule {}
